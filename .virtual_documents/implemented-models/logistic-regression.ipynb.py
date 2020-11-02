91f25168bb825ae9a5d270ea4a4a98d68a5f2775
Andrei Alexandru <inwaves@live.com>
2020-11-02 23:34:33 +0200

    Partial dependence plots

--- .virtual_documents/implemented-models/logistic-regression.ipynb.py
+++ .virtual_documents/implemented-models/logistic-regression.ipynb.py
@@ -1,10 +1,11 @@
 import numpy as np # linear algebra
 import pandas as pd # data manipulation
 import matplotlib.pyplot as plt # visualisation
-import altair as alt
-import seaborn as sns
-import eli5
-from IPython.display import display
+import altair as alt # visualisation
+import seaborn as sns # visualisation
+import eli5 # for permutation importance
+from pdpbox import pdp, get_dataset, info_plots # for partial dependence plotting 
+from IPython.display import display # for permutation importance display
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score, recall_score, accuracy_score, roc_auc_score, plot_confusion_matrix
@@ -52,39 +53,65 @@ y = diabetes_df['outcome']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0, shuffle=True)
 
 
-def print_results(lin_model, predictions_lin_model, solver):
+weights = []
+def show_model_permutations(lin_model=None):
+    """ Appends permutation importance for the labels on one model to a list.
+        Prints when called with no model.
+    """
+    if lin_model==None:         
+        for display_weights in weights:
+            display(display_weights)
+        return
+    
+    perm_importance = calc_model_perm_importance(lin_model)
+    weights.append(eli5.show_weights(perm_importance, feature_names = X_test.columns.tolist()))
+    
+def calc_model_perm_importance(lin_model):
+    """ Calculates the permutation importance for the labels on one model. """
+    return eli5.sklearn.PermutationImportance(lin_model,
+                                                        random_state=1).fit(X_test, y_test)
+
+def print_results(lin_model, predictions_lin_model, solver,
+                 confusion_matrix=False):
     print(solver)
     print("-----------------")
-    disp = plot_confusion_matrix(lin_model, X_test, y_test,
-                                display_labels=['positive', 'negative'],
-                                cmap=plt.cm.Blues,
-                                values_format= '.0f')
-    disp.ax_.set_title("Confusion matrix")
-    plt.show()
+    if confusion_matrix:
+        disp = plot_confusion_matrix(lin_model, X_test, y_test,
+                                    display_labels=['positive', 'negative'],
+                                    cmap=plt.cm.Blues,
+                                    values_format= '.0f')
+        disp.ax_.set_title("Confusion matrix")
+        plt.show()
     print("Accuracy: {}".format(accuracy_score(y_test, predictions_lin_model)))
     print("Precision: {}".format(precision_score(y_test, predictions_lin_model)))
     print("Recall: {}".format(recall_score(y_test, predictions_lin_model)))
     print("ROC AUC: {} \n".format(roc_auc_score(y_test, predictions_lin_model)))
 
+def show_partial_dep_plots(lin_model):
+    for feat_name in X.columns:
+        pdp_dist = pdp.pdp_isolate(model=lin_model, dataset=X_test, 
+                                   model_features=X.columns, feature=feat_name)
+        pdp.pdp_plot(pdp_dist, feat_name)
+        plt.show() 
+    
 def parameterise_model(solver, C=1.0, max_iter=100):
     lin_model = LogisticRegression(solver=solver, random_state=0,
                               C=C, max_iter=max_iter)
+    
     lin_model.fit(X_train, y_train)
+    
     return lin_model
 
-weights = []
-for solver in ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']:
-    lin_model = parameterise_model(solver, C=0.8,
-                                    max_iter=1000)
+def logistic_regression():
+    for solver in ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']:
+        lin_model = parameterise_model(solver, C=0.8,
+                                        max_iter=1000)
+        show_model_permutations(lin_model)
+        
+        predictions_lin_model = lin_model.predict(X_test)
+        print_results(lin_model, predictions_lin_model, solver)
     
-    # Looking at permutation importance for each label
-    perm_importance = eli5.sklearn.PermutationImportance(lin_model,
-                                                        random_state=1).fit(X_test, y_test)
-    weights.append(eli5.show_weights(perm_importance, feature_names = X_test.columns.tolist()))
+    show_partial_dep_plots(parameterise_model(solver='newton-cg'))
+    show_model_permutations() # comment out if you don't want to see permutation importance 
 
-    predictions_lin_model = lin_model.predict(X_test)
-    print_results(lin_model, predictions_lin_model, solver)
-    
-for display_weights in weights:
-    display(display_weights)
-    
+logistic_regression()
