b3f0c8183a5478929f8918452439779a197df706
Andrei Alexandru <inwaves@live.com>
2020-11-03 22:49:51 +0200

    Logistic regression beginning-to-end

--- implemented-models/logistic-regression-implemented.py
+++ implemented-models/logistic-regression-implemented.py
@@ -11,47 +11,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score, recall_score, accuracy_score, roc_auc_score, plot_confusion_matrix
 
 
-diabetes_df = pd.read_csv('../data/diabetes-classification.csv')
+def preprocess_data():
+    """ Pre-processes the dataset with common data cleaning methods.
+    """
 
-diabetes_df.columns = [col.lower() for col in diabetes_df.columns]
+    diabetes_df = pd.read_csv('../data/diabetes-classification.csv')
 
+    diabetes_df.columns = [col.lower() for col in diabetes_df.columns]
 
-diabetes_df.info()
+    X = diabetes_df.drop(['outcome', 'age', 'skinthickness'], axis=1) # dropping correlated variables
+    y = diabetes_df['outcome']
 
-
-diabetes_df.corr()
-
-
-# plt.figure(figsize=(12, 7))
-# sns.boxplot(data=diabetes_df, x='outcome', y='bmi', palette='BuPu')
-
-alt.Chart(diabetes_df).mark_bar().encode(
-    x = 'outcome:O',
-    y = 'count()'
-).properties(width=150)
-
-
-alt.Chart(diabetes_df).mark_bar(width=10).encode(
-    alt.X('pregnancies:Q'),
-    alt.Y('count():Q'))
-
-
-alt.Chart(diabetes_df[diabetes_df['outcome']==1]).mark_bar().encode(
-    x = alt.X('age:Q'),
-    y = alt.Y('count():Q'),
-    tooltip = [alt.Tooltip('age:Q'),
-              alt.Tooltip('count()')]
-). properties(
-                                width=600,
-                                height=300)
-
-
-X = diabetes_df.drop(['outcome', 'age', 'skinthickness'], axis=1)
-y = diabetes_df['outcome']
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0, shuffle=True)
-
+    train_test_split(X, y, test_size=0.2, random_state=0, shuffle=True)
 
 weights = []
 def show_model_permutations(lin_model=None):
@@ -67,12 +38,16 @@ def show_model_permutations(lin_model=None):
     weights.append(eli5.show_weights(perm_importance, feature_names = X_test.columns.tolist()))
     
 def calc_model_perm_importance(lin_model):
-    """ Calculates the permutation importance for the labels on one model. """
+    """ Calculates the permutation importance for the labels on one model. 
+    """
+
     return eli5.sklearn.PermutationImportance(lin_model,
                                                         random_state=1).fit(X_test, y_test)
 
 def print_results(lin_model, predictions_lin_model, solver,
                  confusion_matrix=False):
+    """ Prints the confusion matrix and precision, accuracy, recall and ROC AUC.
+    """
     print(solver)
     print("-----------------")
     if confusion_matrix:
@@ -87,14 +62,18 @@ def print_results(lin_model, predictions_lin_model, solver,
     print("Recall: {}".format(recall_score(y_test, predictions_lin_model)))
     print("ROC AUC: {} \n".format(roc_auc_score(y_test, predictions_lin_model)))
 
-def show_partial_dep_plots(lin_model):
-    for feat_name in X.columns:
+def show_partial_dep_plots(lin_model, X_test):
+    """ Prints partial dependence plots for each feature in the dataset.
+    """
+    for feat_name in X_test.columns:
         pdp_dist = pdp.pdp_isolate(model=lin_model, dataset=X_test, 
-                                   model_features=X.columns, feature=feat_name)
+                                   model_features=X_test.columns, feature=feat_name)
         pdp.pdp_plot(pdp_dist, feat_name)
         plt.show() 
     
-def parameterise_model(solver, C=1.0, max_iter=100):
+def parameterise_model(X_train, y_train, solver, C=1.0, max_iter=100):
+    """ Generates and fits a logistic regression model
+    """
     lin_model = LogisticRegression(solver=solver, random_state=0,
                               C=C, max_iter=max_iter)
     
@@ -102,20 +81,24 @@ def parameterise_model(solver, C=1.0, max_iter=100):
     
     return lin_model
 
-def logistic_regression():
+def logistic_regression(X_train, X_test, y_train, y_test):
+    """ Spawns several logistic regression models, displays their permutation importances, then scores their predictions.
+    """
     for solver in ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']:
-        lin_model = parameterise_model(solver, C=0.8,
+        lin_model = parameterise_model(X_train, y_train,
+                                        solver, C=0.8,
                                         max_iter=1000)
         show_model_permutations(lin_model)
         
         predictions_lin_model = lin_model.predict(X_test)
         print_results(lin_model, predictions_lin_model, solver)
     
-    show_partial_dep_plots(parameterise_model(solver='newton-cg'))
+    show_partial_dep_plots(parameterise_model(X_train, y_train, solver='newton-cg'), X_test)
     show_model_permutations() # comment out if you don't want to see permutation importance 
 
 def main():
-    logistic_regression()
+    X_train, X_test, y_train, y_test = preprocess_data()
+    logistic_regression(X_train, X_test, y_train, y_test)
     
 if __name__ == '__main__':
     main()
